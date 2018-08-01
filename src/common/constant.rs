0845c47a560b3441d9a0b57c5ed792be1113635b
zero.qn <zeroqn@users.noreply.github.com>
2018-08-01 22:29:44 +0800

    Ignore unused ERR_MSG_BAD_JSON_PAYLOAD constant

--- src/common/constant.rs
+++ src/common/constant.rs
@@ -1,6 +1,6 @@
 pub const CONTENT_TYPE_JSON: &str = "application/json";
 pub const ERR_MSG_BAD_ID: &str = "bad id";
-pub const ERR_MSG_BAD_JSON_PAYLOAD: &str = "bad json payload";
+pub const _ERR_MSG_BAD_JSON_PAYLOAD: &str = "bad json payload";
 pub const ERR_MSG_DATA_NOT_FOUND: &str = "data no found";
 pub const ERR_MSG_DATABASE_OPEARTION_FAIL: &str = "database operation fail";
 pub const ERR_MSG_PAYLOAD_PARSE_ORDERBY_FAIL: &str = "parse orderby list fail";
